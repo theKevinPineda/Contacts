860e54701ded577b7718facf593ad2f63686e6b9
Raphael Pineda <dakmatz495@yahoo.com>
2012-12-03 13:59:46 +0800

    Edit Contact in Database (Running)
    EditActivity can now update the database
    but cannot refresh the shown rows in listview meaning it won't update
    the updated contact yet

--- src/com/cs175/androidsqlite/EditActivity.java
+++ src/com/cs175/androidsqlite/EditActivity.java
@@ -2,6 +2,9 @@ package com.cs175.androidsqlite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class EditActivity extends Activity {
@@ -14,11 +17,47 @@ private Contact recievedContact;
         Bundle b = getIntent().getExtras();
         recievedContact = (Contact) b.getSerializable("contact");
         
-        EditText editName = (EditText) findViewById(R.id.newName);
-        EditText editNumber = (EditText) findViewById(R.id.newNumber);
+        final EditText editName = (EditText) findViewById(R.id.newName);
+        final EditText editNumber = (EditText) findViewById(R.id.newNumber);
+        final Button edit = (Button) findViewById(R.id.editButton);
+        final Button cancel = (Button) findViewById(R.id.cancelButton);
+        
+        
         editName.setHint(recievedContact.getName());
         editNumber.setHint(recievedContact.getPhoneNumber());
         
+        edit.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				pressedEdit(editName, editNumber);
+			}
+		});
+
+        cancel.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				pressedCancel();
+			}
+		});
+        
+        
 	}
 	
+	public void pressedEdit(EditText name, EditText number){
+		String tempName = name.getText().toString()
+			  ,tempNumber =number.getText().toString();
+		if(tempName.isEmpty()!=true)
+		recievedContact.setName(tempName);
+		if(tempNumber.isEmpty()!=true)
+		recievedContact.setPhoneNumber(number.getText().toString());
+		
+		DatabaseHandler db = new DatabaseHandler(this);
+		db.updateContact(recievedContact);
+		finish();
+		
+	}
+	public void pressedCancel(){
+		finish();
+		
+	}
 }
